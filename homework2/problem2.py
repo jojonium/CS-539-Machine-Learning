84162ec1de69ced37e784bc2dbbea66c15714be5
Joseph Petitti <joseph@petitti.org>
2021-03-28 02:19:04 -0400

    finish compute_dz_dW

--- homework2/problem2.py
+++ homework2/problem2.py
@@ -123,7 +123,8 @@ def compute_dL_db(dL_dz, dz_db):
 def compute_dz_dW(x, c):
     #########################################
     ## INSERT YOUR CODE HERE (1 points)
-    
+    dz_db: np.ndarray = np.diag(np.full(c, 1))
+    dz_dW = [[x if j == 1 else np.zeros((len(x))) for j in i] for i in dz_db]
     #########################################
     return dz_dW
     #-----------------
